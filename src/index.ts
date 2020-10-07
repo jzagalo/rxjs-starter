c676ccfbb9117c3875c5cd3be807ecd270be2b62
Paul Ezaga <paulezaga@gmail.com>
2020-10-07 19:41:47 +0200

    Starting with RxJs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
+import { Observable } from "rx";
 // tslint:disable: no-console
 
-import { from } from "rxjs";
-import { map } from "rxjs/operators";
+import { from, of } from "rxjs";
+import { reduce } from "rxjs/operators";
 
 const people: string[] = ["Micheal", "Jim", "Dwight"];
 
-from(people)
-	.pipe(map((name) => `${name} says hi!`))
-	.subscribe((message) => console.log(message));
+
+
+class Calculator {
+	static observerStatic(itemA: number, itemB: number){
+		let obs = from([itemA, itemB]);
+		const sum$ = obs.pipe(reduce((acc: number, item: number) => acc + item))
+		return sum$;
+	}
+}
+
+class Receipt {
+	constructor(observable$: any){
+		observable$.subscribe((value: any) => console.log(`total receipt: ${value}`));
+	}
+}
+
+const pizza = 6.00;
+const beer = 5.00;
+
+const calc = Calculator.observerStatic(pizza, beer);
+const reciever = new Receipt(calc);
+//const receipt = new Receipt(calc.observable);
