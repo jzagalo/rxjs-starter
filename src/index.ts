2cbacf7f4ca9789ba6ab6577de1da96e81809737
Paul Ezaga <paulezaga@gmail.com>
2020-10-12 10:19:11 +0200

    Progress Indicator

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import { Observable } from "rx";
+import { Observer } from "rx";
+import { Observable } from "rxjs";
 // tslint:disable: no-console
 
-import { from, of } from "rxjs";
-import { reduce } from "rxjs/operators";
+import { from, fromEvent, of, Subscriber } from "rxjs";
+import { reduce, map, scan } from "rxjs/operators";
 
 const people: string[] = ["Micheal", "Jim", "Dwight"];
-
-
 
 class Calculator {
 	static observerStatic(itemA: number, itemB: number){
@@ -27,4 +26,50 @@ const beer = 5.00;
 
 const calc = Calculator.observerStatic(pizza, beer);
 const reciever = new Receipt(calc);
-//const receipt = new Receipt(calc.observable);
+
+const link = document.getElementById("google");
+const clickStream = fromEvent(link, "click")
+					.pipe(
+						map((event: any) => { event.preventDefault; 
+							return event.currentTarget.getAttribute("href")})
+					).subscribe(result => console.log(result));
+				
+
+const observable = new Observable((subscriber: any) => {
+	subscriber.next(1);
+	subscriber.next(2);
+	subscriber.next(3);
+
+	setTimeout(() => {
+		subscriber.next(4);
+		subscriber.complete();
+	}, 1000);
+});
+
+observable.subscribe(console.log);
+
+const progressBar$ = new Observable((observer) => {
+	const OFFSET = 3000;
+	const SPEED = 100;
+
+	let val = 0;
+	function progress(){
+		if(++val <= 100){
+			console.log(++val, val);
+			observer.next(val);
+			setTimeout(progress, SPEED);
+		} else {
+			observer.complete();
+		}		
+	}
+
+	setTimeout(progress, OFFSET);
+});
+
+const label = document.getElementById('tell');
+
+progressBar$.subscribe(
+	val => label.textContent = (Number.isInteger(val) ? val + "%" : val.toString()),
+	error => console.log(error.message),
+	() => label.textContent = "Complete"
+)
