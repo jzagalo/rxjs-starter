bcb2c3241890dec36bc8c69a2d2264adb72f58b8
Paul Ezaga <paulezaga@gmail.com>
2020-10-12 18:33:35 +0200

    Stockmarket like with RxJS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Observer } from "rx";
-import { Observable } from "rxjs";
+import { interval, Observable } from "rxjs";
 // tslint:disable: no-console
 
 import { from, fromEvent, of, Subscriber } from "rxjs";
-import { reduce, map, scan } from "rxjs/operators";
+import { reduce, map, pluck, tap, filter, skip, take, timeInterval } from "rxjs/operators";
 
-const people: string[] = ["Micheal", "Jim", "Dwight"];
+/* const people: string[] = ["Micheal", "Jim", "Dwight"];
 
 class Calculator {
 	static observerStatic(itemA: number, itemB: number){
@@ -54,8 +54,7 @@ const progressBar$ = new Observable((observer) => {
 
 	let val = 0;
 	function progress(){
-		if(++val <= 100){
-			console.log(++val, val);
+		if(++val <= 100){		
 			observer.next(val);
 			setTimeout(progress, SPEED);
 		} else {
@@ -73,3 +72,73 @@ progressBar$.subscribe(
 	error => console.log(error.message),
 	() => label.textContent = "Complete"
 )
+
+const computerFuturesValue = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(42);
+	}, 5000)
+});
+
+const promise$ = from(computerFuturesValue)
+					.subscribe(
+						val => console.log(val),
+						err => console.log(`${err}`),
+						() => console.log(" All done") 
+					);*/
+const rxdo$ = from([
+	'The quick brown fox',
+	'jumps over the lazy dogs'
+]). pipe(
+	map(str => str.split(' ')),
+	tap(arr => console.log(arr.length)),
+
+).subscribe(console.log)
+
+const isNumericalKeyCode = (code: number) => code >= 48 && code <= 57;
+const input = document.getElementById("input");
+const inputObs$ = fromEvent(input, 'keyup')
+					.pipe(
+						pluck('keyCode'),
+						filter(isNumericalKeyCode),
+					).subscribe(code => console.log(`User typed: ${String.fromCharCode(code)}`))
+
+const newRandomNumber = () => Math.floor(Math.random() * 100);
+
+const Money = function(currency: string, val: number){
+	return {
+		value: function(){
+			return val;
+		},
+		currency: function(){
+			return currency;
+		},
+		toString: function(){
+			return `${currency} ${val}`;
+		},
+	}
+};
+
+const currencyObs$ = interval(2000)					
+					.pipe(
+						timeInterval(),
+						skip(1),
+						take(10),
+						tap(int => console.log(`Checking every ${int.interval} milliseconds`)),
+						map(num => Money("USD", newRandomNumber()),
+					));
+
+currencyObs$.subscribe(
+	(price: any) => document.getElementById('price').textContent = price
+);
+
+
+
+
+
+
+
+
+
+
+
+
